d0e5ca66793ddcc6799dca20b21c6fd167b58356
YF <58352097+YFL-dev@users.noreply.github.com>
2023-12-26 15:58:22 -0800

    finished assignment 02 (#3)
    * finished assignment 02
    
    * removed .vscode folder

--- assignment_02/03.js
+++ assignment_02/03.js
@@ -2,3 +2,18 @@
  * Write a function to find the area and perimeter of a circle
  * take radius as an argument for the function call
  */
+
+function calculateCircleArea(radius) {
+  let circleArea = Math.PI * radius * radius;
+  return circleArea;
+}
+
+function calculateCirclePerimeter(radius) {
+  let circlePerimeter = 2 * Math.PI * radius;
+  return circlePerimeter;
+}
+
+let radius = 3;
+
+console.log(calculateCircleArea(radius));
+console.log(calculateCirclePerimeter(radius));
