d7f7bb2ad63a6e44562888a8fe251304abc390e7
Joyce W <jianjoycewu@gmail.com>
2023-12-26 18:55:31 -0800

    multiple funcs use the same test suite

--- assignment_04/src/01.js
+++ assignment_04/src/01.js
@@ -1,12 +1,12 @@
 /**
  * Write a function to sum 1 to n,
  * return the result.
- * num: integer or string integer 
- * 
+ * num: integer or string integer
+ *
  */
 
 const sum = (num) => {
-  num = Number(num)
+  num = Number(num);
 
   if (num < 1 || num % 1 !== 0) {
     throw new Error('Invalid input: argument must be a positive integer.');
@@ -16,7 +16,6 @@ const sum = (num) => {
     throw new Error('Invalid input: argument must be a positive integer.');
   }
 
-
   let total = 0;
   for (let i = 1; i < num + 1; i++) {
     total += i;
@@ -25,6 +24,34 @@ const sum = (num) => {
   return total;
 };
 
+/**
+ * Write a function to sum 1 to n,
+ * return the result.
+ */
+
+const sum1 = (num) => {
+  const parsedNum = parseInt(num, 10);
+
+  // Check if the original input was a non-integer number or if it's NaN
+  if (isNaN(parsedNum) || parsedNum < 1 || parsedNum !== parseFloat(num)) {
+    throw new Error('Invalid input: argument must be a positive integer.');
+  }
+
+  return (parsedNum * (parsedNum + 1)) / 2;
+};
+
+const sum2 = (num) => {
+  num = Number(num);
+
+  if (!Number.isInteger(num) || num < 1) {
+    throw new Error('Invalid input: argument must be a positive integer.');
+  }
+
+  return (num * (num + 1)) / 2;
+};
+
 module.exports = {
   sum,
+  sum1,
+  sum2,
 };
--- assignment_04/tests/01.test.js
+++ assignment_04/tests/01.test.js
@@ -1,30 +1,36 @@
-const { sum } = require('../src/01');
+const { sum, sum1, sum2 } = require('../src/01');
 
-describe('sum', () => {
+const functionsToTest = [
+  { name: 'sum', func: sum },
+  { name: 'sum1', func: sum1 },
+  { name: 'sum2', func: sum2 },
+];
+
+describe.each(functionsToTest)('$name', ({ func }) => {
   test('add 1 to 100 to equal 5050', () => {
-    expect(sum(100)).toBe(5050);
+    expect(func(100)).toBe(5050);
   });
 
-  test('add 1 to 100 to equal 5050', () => {
-    expect(sum('100')).toBe(5050);
+  test('add 1 to "100" to equal 5050', () => {
+    expect(func('100')).toBe(5050);
   });
 
-  test('add 1 to 100 to equal 5050', () => {
-    expect(sum('100.0')).toBe(5050);
+  test('add 1 to "100.0" to equal 5050', () => {
+    expect(func('100.0')).toBe(5050);
   });
 
-  test('add 1 to 100 to equal 5050', () => {
-    expect(sum(100.0)).toBe(5050);
+  test('add 1 to 100.0 to equal 5050', () => {
+    expect(func(100.0)).toBe(5050);
   });
 
   test('throws error when input is invalid', () => {
-    expect(() => sum(-100)).toThrow(
+    expect(() => func(-100)).toThrow(
       'Invalid input: argument must be a positive integer.'
     );
-    expect(() => sum('a')).toThrow(
+    expect(() => func('a')).toThrow(
       'Invalid input: argument must be a positive integer.'
     );
-    expect(() => sum(11.02)).toThrow(
+    expect(() => func(11.02)).toThrow(
       'Invalid input: argument must be a positive integer.'
     );
   });
